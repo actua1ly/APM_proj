13465140c106ae01aa774afc34e030222b884b09
Joshua Weiss <jxw7470@g.rit.edu>
2018-10-07 20:39:39 -0400

    functionality for collection
    added some functionality for collection, it is untested, so unsure if it works

--- apm_collect.sh
+++ apm_collect.sh
@@ -2,7 +2,13 @@
 
 #loops untill 900 seconds or 15 minutes has passed
 while [ "$SECONDS" -lt 900 ]; do
-  # data collection functionality will go here
+  # append a line that reads $SECONDS, <%cpu>, <%gpu>
+  "$SECONDS" ", " "$(ps -p "$PID1" %CPU)" ", " "$(ps -p "$PID1" %gpu)" >> APM1.txt
+  "$SECONDS" ", " "$(ps -p "$PID2" %CPU)" ", " "$(ps -p "$PID2" %gpu)" >> APM2.txt
+  "$SECONDS" ", " "$(ps -p "$PID3" %CPU)" ", " "$(ps -p "$PID3" %gpu)" >> APM3.txt
+  "$SECONDS" ", " "$(ps -p "$PID4" %CPU)" ", " "$(ps -p "$PID4" %gpu)" >> APM4.txt
+  "$SECONDS" ", " "$(ps -p "$PID5" %CPU)" ", " "$(ps -p "$PID5" %gpu)" >> APM5.txt
+  "$SECONDS" ", " "$(ps -p "$PID6" %CPU)" ", " "$(ps -p "$PID6" %gpu)" >> APM6.txt
   #since we only want to collect every five seconds
   sleep 5
 done
